92dad1d26342132df355ec80aee90e0d53d3be02
jdigi <jason.digiacobbe@gmail.com>
2024-04-09 18:19:43 -0400

    added jobs route

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import express from "express";
 import cors from "cors";
 // import the routes
 import records from "./routes/record.js";
+import jobs from "./routes/job.js";
 
 // set port via environment variable or default to 5050
 const PORT = process.env.PORT || 5050;
@@ -18,6 +19,8 @@ app.use(express.json());
 
 // use the records route
 app.use("/record", records);
+// use the jobs route
+app.use("/job", jobs);
 
 // listen on the port
 app.listen(PORT, () => {
