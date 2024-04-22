25d498d1d840837ebc02bc0a119d2598e02c5d83
jdigi <jason.digiacobbe@gmail.com>
2024-04-22 17:51:21 -0400

    added tracker route

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import cors from "cors";
 // import the routes
 import transactions from "./routes/transaction.js";
 import accounts from "./routes/account.js";
+import trackers from "./routes/tracker.js";
 
 // set port via environment variable or default to 5050
 const PORT = process.env.PORT || 5050;
@@ -17,10 +18,12 @@ app.use(cors());
 // use json
 app.use(express.json());
 
-// use the records route
+// use the transaction route
 app.use("/transaction", transactions);
-// use the jobs route
+// use the account route
 app.use("/account", accounts);
+// use the trackers route
+app.use("/tracker", trackers);
 
 // listen on the port
 app.listen(PORT, () => {
